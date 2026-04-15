79fc8d2b45c62c86df6027ec352daee8e234f428
wenyan <do_yzr@163.com>
2026-04-15 23:06:35 +0800

    添加: giscus

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,14 +4,11 @@ import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { inject } from "@vercel/analytics";
 import "./style.css";
+import MyLayout from "./MyLayout.vue";
 
 export default {
     extends: DefaultTheme,
-    Layout: () => {
-        return h(DefaultTheme.Layout, null, {
-            // https://vitepress.dev/guide/extending-default-theme#layout-slots
-        });
-    },
+    Layout: MyLayout,
     enhanceApp({ app, router, siteData }) {
         // Initialize Vercel Analytics
         if (typeof window !== "undefined") {
